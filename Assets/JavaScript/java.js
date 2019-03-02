cd82f8b91040f35b09a22b3d8b876cd8d75bf320
candye_sweet <olyamelska@dyn-209-2-212-233.dyn.columbia.edu>
2019-03-02 10:02:38 -0500

    Database

--- Assets/JavaScript/java.js
+++ Assets/JavaScript/java.js
@@ -1,41 +1,97 @@
 let database = firebase.database();
-var connectionsRef = database.ref("/connections");
-var connectedRef = database.ref(".info/connected");
-console.log(database);
+let connectionsRef = database.ref("/connections");
+let connectedRef = database.ref(".info/connected");
+let initialValue = 2;
+let clickCounter = initialValue;
 
 connectedRef.on("value", function(snap) {
-  // If they are connected..
   if (snap.val()) {
-    // Add user to the connections list.
     var con = connectionsRef.push(true);
-
-    // Remove user from the connection list when they disconnect.
     con.onDisconnect().remove();
   }
 });
 
+database.ref("/clicks").on(
+  "value",
+  function(snapshot) {
+    console.log(snapshot.val());
+    clickCounter = snapshot.val().clickCount;
+    console.log(clickCounter);
+  },
+  function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  }
+);
+
+// --------------------------------------------------------------
+
 $("#selectPlayer1").on("click", event => {
   $("#player1").removeClass("hidden");
   $(".main-page").attr("class", "hidden");
-  database.ref().push({
-    player1: true
+
+  clickCounter--;
+
+  // Alert User and reset the counter
+  if (clickCounter === 0) {
+    alert("Yay, two players are playing!");
+    clickCounter = initialValue;
+    selectWeapon();
+    playTheGame();
+  }
+
+  // Save new value to Firebase
+  database.ref("/clicks").set({
+    clickCount: clickCounter
   });
+
+  // Log the value of clickCounter
+  console.log(clickCounter);
 });
 
 $("#selectPlayer2").on("click", event => {
   $("#player2").removeClass("hidden");
   $(".main-page").attr("class", "hidden");
-  database.ref().push({
-    player2: true
+
+  clickCounter--;
+
+  // Alert User and reset the counter
+  if (clickCounter === 0) {
+    alert("Yay, two players are playing!");
+    clickCounter = initialValue;
+    selectWeapon();
+    playTheGame();
+  }
+
+  // Save new value to Firebase
+  database.ref("/clicks").set({
+    clickCount: clickCounter
   });
+
+  // Log the value of clickCounter
+  console.log(clickCounter);
 });
 
-$(".images").on("click", event => {
-  let weapon = $(event.target).attr("value");
-  $("#displayChoice1").removeClass("hidden");
-  $("#weapon1").text(weapon);
-  $("#displayChoice2").removeClass("hidden");
-  $("#weapon2").text(weapon);
-});
+function selectWeapon() {
+  $(".images1").on("click", event => {
+    let weapon = $(event.target).attr("value");
+    $("#displayChoice1").removeClass("hidden");
+    $("#weapon1").text(weapon);
+    database.ref().push({
+      player1: weapon
+    });
+  });
+  $(".images2").on("click", event => {
+    let weapon = $(event.target).attr("value");
+    $("#displayChoice2").removeClass("hidden");
+    $("#weapon2").text(weapon);
+    database.ref().push({
+      player2: weapon
+    });
+  });
+}
 
-$("#submit").on("click", event => {});
+function playTheGame() {
+  $("#submit").on("click", event => {
+    console.log("You are playing the game!");
+  });
+}
